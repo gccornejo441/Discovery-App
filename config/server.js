47b4395b924393badbcb14b01cc835a010d84db1
unknown <gcornejo441@gmail.com>
2021-06-04 23:06:10 -0700

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'afdbdbd7e638980dd18423660c8cecc2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6dcd9eea-b80c-40e7-9f19-2a7a9301a722'
+};
